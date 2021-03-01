1fb6ac974a1fcab57d98e24fd4f42ce4bc9ff12a
avikr <avikr@LAPTOP-SN1MFHTP>
2021-03-01 13:40:31 +0530

    added class as tset.java

---
+++ TestProject/src/Tset.java
@@ -0,0 +1,19 @@
+
+public class Tset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		{
+
+		    // create a string
+		    String greet = "Hello! World";
+		    System.out.println("The string is: " + greet);
+
+		    //checks the string length
+		    System.out.println("The length of the string: " + greet.length());
+		  }
+
+	}
+
+}
